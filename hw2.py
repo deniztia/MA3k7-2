03acbbed950c692750dc906be4b65029ffbcdf45
deniztia <156916383+deniztia@users.noreply.github.com>
2024-01-18 15:22:57 +0000

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -8,14 +8,14 @@ if num[1] < 10:
     num[2] = int(input('Enter second number:'))
     
     if num[2] < 10:
-        for i in range(3, 11):
+        for i in range(3, 13):
             num[i] = num[i-2] + num[i-1]
             if num[i] > 9:
                 num[i] -= 10
     else:
         num[2] = int(input('The number must be an integer less than 10, enter a number:'))
         
-        for i in range(3, 11):
+        for i in range(3, 13):
             num[i] = num[i-2] + num[i-1]
             if num[i] > 9:
                 num[i] -= 10
@@ -26,19 +26,19 @@ else:
         num[2] = int(input('Enter second number:'))
         
         if num[2] < 10:
-            for i in range(3, 11):
+            for i in range(3, 13):
                 num[i] = num[i-2] + num[i-1]
                 if num[i] > 9:
                     num[i] -= 10
         else:
             num[2] = int(input('The number must be an integer less than 10, enter a number:'))
             
-            for i in range(3, 11):
+            for i in range(3, 13):
                 num[i] = num[i-2] + num[i-1]
                 if num[i] > 9:
                     num[i] -= 10
     else:
         print('Both numbers must be less than 10.')
 
-for i in range(1, 11):
+for i in range(1, 13):
     print(num[i])
