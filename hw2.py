0dfbdf3ab54d51d65c6fdc8345d1c1e3b4f16e5c
deniztia <156916383+deniztia@users.noreply.github.com>
2024-01-22 10:49:54 +0000

    Update hw2.py

--- hw2.py
+++ hw2.py
@@ -1,44 +1,28 @@
-import numpy as np
+def generate_bracelet(num1, num2):
+    bracelet = [num1, num2]
 
-num = {}  # Use a dictionary to store the numbers
-
-num[1] = int(input('Enter a number:'))
-
-if num[1] < 10:
-    num[2] = int(input('Enter second number:'))
-    
-    if num[2] < 10:
-        for i in range(3, 13):
-            num[i] = num[i-2] + num[i-1]
-            if num[i] > 9:
-                num[i] -= 10
-    else:
-        num[2] = int(input('The number must be an integer less than 10, enter a number:'))
+    while True:
+        next_num = (bracelet[-1] + bracelet[-2]) % 10
+        bracelet.append(next_num)
         
-        for i in range(3, 13):
-            num[i] = num[i-2] + num[i-1]
-            if num[i] > 9:
-                num[i] -= 10
-else:
-    num[1] = int(input('The number must be an integer less than 10, enter a number:'))
+        # Check for repetition
+        if bracelet[-2:] == [num1, num2]:
+            break
 
-    if num[1] < 10:
-        num[2] = int(input('Enter second number:'))
-        
-        if num[2] < 10:
-            for i in range(3, 13):
-                num[i] = num[i-2] + num[i-1]
-                if num[i] > 9:
-                    num[i] -= 10
-        else:
-            num[2] = int(input('The number must be an integer less than 10, enter a number:'))
-            
-            for i in range(3, 13):
-                num[i] = num[i-2] + num[i-1]
-                if num[i] > 9:
-                    num[i] -= 10
-    else:
-        print('Both numbers must be less than 10.')
+    return bracelet
 
-for i in range(1, 13):
-    print(num[i])
+# Get user input for num1
+num1 = int(input("Enter the first number (less than 10): "))
+while num1 >= 10:
+    num1 = int(input("The number must be an integer less than 10, enter a valid number: "))
+
+# Get user input for num2
+num2 = int(input("Enter the second number (less than 10): "))
+while num2 >= 10:
+    num2 = int(input("The number must be an integer less than 10, enter a valid number: "))
+
+result_bracelet = generate_bracelet(num1, num2)
+length=  len(result_bracelet)-2
+# Print the bracelet and its length
+print("Number Bracelet:", result_bracelet)
+print("Length of Bracelet:", length)
